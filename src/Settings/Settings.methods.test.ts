4d884879a6c72346f3080b9bf5e228673da6bae5
tomgodee <trungvu8295@gmail.com>
2025-04-10 20:49:45 +0700

    Swap subscribe and test in unit test

--- src/Settings/Settings.methods.test.ts
+++ src/Settings/Settings.methods.test.ts
@@ -1,13 +1,13 @@
 import { expect, test, describe } from "vitest";
 import { isPositiveDigit } from "./Settings.methods";
 
-test("isPositiveDigit", () => {
-  describe("should return a match array if the string is a positive digit", () => {
+describe("isPositiveDigit", () => {
+  test("should return a match array if the string is a positive digit", () => {
     expect(isPositiveDigit("1")).toHaveLength(1);
     expect(isPositiveDigit("10")).toHaveLength(1);
   });
 
-  describe("should return null if the string is not a positive digit", () => {
+  test("should return null if the string is not a positive digit", () => {
     expect(isPositiveDigit("1.0")).toBeNull();
     expect(isPositiveDigit("-1")).toBeNull();
     expect(isPositiveDigit("abc")).toBeNull();
